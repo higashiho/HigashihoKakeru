128fa8e96397dc6238e407e0575740128a378c97
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-11-09 14:58:48 +0900

    TaskB編集
    TaskBを新規作成。BをCに、CをDに変更

---
+++ 20221102/TaskB .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int value = 0;
+    int maxValue = 10;
+
+    while(value < maxValue){
+        value++;
+        printf("%d\n", value);
+    }
+
+    return 0;
+}
